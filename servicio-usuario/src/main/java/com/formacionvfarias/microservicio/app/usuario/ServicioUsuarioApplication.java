faf8c304f6740a5bc58f77ebf769f827e8e3dd59
Vfari <Vfari@DESKTOP-RCV14BB>
2020-11-15 16:22:25 -0300

    Se agrega notación que define a microservicio-usuario como cliente de eureka.

--- servicio-usuario/src/main/java/com/formacionvfarias/microservicio/app/usuario/ServicioUsuarioApplication.java
+++ servicio-usuario/src/main/java/com/formacionvfarias/microservicio/app/usuario/ServicioUsuarioApplication.java
@@ -2,7 +2,10 @@ package com.formacionvfarias.microservicio.app.usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class ServicioUsuarioApplication {
 

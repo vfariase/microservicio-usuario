a102b31faf9f4026b4260555fc55b78944455a6c
Vfari <Vfari@DESKTOP-RCV14BB>
2020-11-15 16:39:22 -0300

    aumentar la cohesión y disminuir la dependencia.

--- servicio-usuario/src/main/java/com/formacionvfarias/microservicio/app/usuario/services/AlumnoServiceImpl.java
+++ servicio-usuario/src/main/java/com/formacionvfarias/microservicio/app/usuario/services/AlumnoServiceImpl.java
@@ -6,38 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.formacionvfarias.microservicio.app.commons.services.CommonServiceImpl;
 import com.formacionvfarias.microservicio.app.usuario.entity.Alumno;
 import com.formacionvfarias.microservicio.app.usuario.repository.AlumnoRepository;
 
 
 @Service
-public class AlumnoServiceImpl implements AlumnoService {
+public class AlumnoServiceImpl extends CommonServiceImpl<Alumno, AlumnoRepository> implements AlumnoService {
 
-	@Autowired
-	private AlumnoRepository repository;
+
 	
-	@Override
-	@Transactional(readOnly = true)
-	public Iterable<Alumno> findAll() {
-		return repository.findAll();
-	}
-
-	@Override
-	@Transactional(readOnly = true)
-	public Optional<Alumno> findById(Long id) {
-        return repository.findById(id);
-	}
-
-	@Override
-	@Transactional(readOnly = true)
-	public Alumno save(Alumno alumno) {
-		return repository.save(alumno);
-	}
-
-	@Override
-	@Transactional(readOnly = true)
-	public void deleteById(Long id) {
-       repository.deleteById(id);
-	}
-
+	
 }
